0b87d292e3214804cc093333fc75c15b5e6750c4
Flávio Arêas <vflavio9@gmail.com>
2021-06-07 21:44:17 -0300

    feat: add CreateTransactionService

---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,36 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface Request {
+	title: string;
+	value: number;
+	type: 'income' | 'outcome';
+}
+
+class CreateTransactionService {
+	private transactionsRepository: TransactionsRepository;
+
+	constructor(transactionsRepository: TransactionsRepository) {
+		this.transactionsRepository = transactionsRepository;
+	}
+
+	public execute({ title, value, type }: Request): Transaction {
+		const balance = this.transactionsRepository.getBalance().total;
+
+		if (type === 'outcome' && balance < value) {
+			throw Error(
+				"You can't create outcome transaction without a valid balance",
+			);
+		}
+
+		const transaction = this.transactionsRepository.create({
+			title,
+			type,
+			value,
+		});
+
+		return transaction;
+	}
+}
+
+export default CreateTransactionService;
