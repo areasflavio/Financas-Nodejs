7b28d61888773d1660859f4dada76e5d0ef7f0ef
Flávio Arêas <vflavio9@gmail.com>
2022-02-14 15:40:39 -0300

    chore: update app config

--- src/database/index.ts
+++ src/database/index.ts
@@ -8,7 +8,7 @@ export default async (name = 'default'): Promise<Connection> => {
 			name,
 			database:
 				process.env.NODE_ENV === 'test'
-					? 'financas_node_tests'
+					? 'fmoney_tests'
 					: defaultOptions.database,
 		}),
 	);
