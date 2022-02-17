6169174a1700cce872ac962df44ad7bcb6a83264
Flávio Arêas <vflavio9@gmail.com>
2022-02-17 14:53:54 -0300

    fix: database config

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,13 +3,34 @@ import { createConnection, getConnectionOptions, Connection } from 'typeorm';
 export default async (name = 'default'): Promise<Connection> => {
 	const defaultOptions = await getConnectionOptions();
 
+	if (process.env.DATABASE_URL) {
+		Object.assign(defaultOptions, {
+			entities: ['./dist/models/*.js'],
+			migrations: ['./dist/database/migrations/*.js'],
+			cli: {
+				migrationsDir: './dist/database/migrations',
+			},
+		});
+	} else {
+		Object.assign(defaultOptions, {
+			entities: ['./src/models/*.ts'],
+			migrations: ['./src/database/migrations/*.ts'],
+			cli: {
+				migrationsDir: './src/database/migrations',
+			},
+		});
+	}
+
 	return createConnection(
 		Object.assign(defaultOptions, {
 			name,
+			type: 'postgres',
 			database:
 				process.env.NODE_ENV === 'test'
 					? 'fmoney_tests'
 					: defaultOptions.database,
+			synchronize: false,
+			logging: false,
 		}),
 	);
 };
